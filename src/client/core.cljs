caeab58ebaed9fe1850e2c1e5d2e6bd944458685
Sam Stiles <samuel.dev.stiles@gmail.com>
2015-07-02 15:46:38 -0300

    Add files, update deps

--- src/client/core.cljs
+++ src/client/core.cljs
@@ -1,27 +1,32 @@
 (ns ^:figwheel-always client.core
-    (:require[om.core :as om :include-macros true]
-              [om.dom :as dom :include-macros true]))
+    (:require [om.core :as om :include-macros true]
+              [om.dom :as dom :include-macros true]
+              [cljs-ajax :as http]))
 
 (enable-console-print!)
 
-(println "Edits to this text should show up in your developer console.")
-
-;; define your app data so that it doesn't get over-written on reload
-
-(defonce app-state (atom {:text "Hello world!"}))
-
 (om/root
   (fn [data owner]
-    (reify om/IRender
-      (render [_]
-        (dom/h1 nil (:text data)))))
-  app-state
+    (reify
+
+      ;; Get initial state
+      om/IInitState
+      (init-state [_]
+        {:text "Hello Bob!"})
+
+      ;; Render
+      om/IRenderState
+      (render-state [_ state]
+        (dom/h1 nil (:text state)))))
+
+  state
+
   {:target (. js/document (getElementById "app"))})
 
 
 (defn on-js-reload []
-  ;; optionally touch your app-state to force rerendering depending on
+  ;; optionally touch your state to force rerendering depending on
   ;; your application
-  ;; (swap! app-state update-in [:__figwheel_counter] inc)
+  ;; (swap! state update-in [:__figwheel_counter] inc)
 )
 
